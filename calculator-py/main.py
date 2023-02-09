856e77bc91e3bf5a630bb731eee8408a535cc8df
Niraj Lekhak <nrajlekhak@gmail.com>
2023-02-09 22:21:56 +0545

    division with remainder

--- calculator-py/main.py
+++ calculator-py/main.py
@@ -38,31 +38,35 @@ def bit_sifted_div(dividend:int, divisor:int):
 
     # when dividend is 0, result of division is also 0
     if dividend == 0:
-        return 0
+        return {'quotient': 0, 'remainder': 0}
 
     # when divisor is 0, result of division is infinity
     if divisor == 0:
-        return 'infinity'
+        return {'quotient':'infinity', 'remainder': None}
     
-    quotient = 0
+    
     sign = -1 if (dividend <0 and divisor>0) or (divisor<0 and dividend>0) else 1
 
     dividend = abs(dividend)
     divisor = abs(divisor)
 
+    quotient = 0
+    remainder = dividend
+
     for i in range(31,-1,-1):
-        if  divisor << i <= dividend:
-            dividend-=divisor << i
+        di = divisor << i
+        if  di <= remainder:
+            remainder-=di
             quotient += 1 <<i
 
     if sign < 0:
         quotient = -quotient
 
-    return quotient 
+    return {'quotient': quotient, 'remainder': remainder } 
 
 
 
 print('bitwise add', bit_sifted_add(10, 1))
 print('bitwise sub', bit_sifted_sub(-9, -10))
 print('bitwise mul', bit_sifted_mul(12, 9))
-print('bitwise div', bit_sifted_div(10, 2))
+print('bitwise div', bit_sifted_div(101, 9))
