8c2a63d1ddcb5687fc76fbfdf097a852ec65ed5a
Neeraj Lekhak <neerajlekhak@lftechnology.com>
2023-02-21 16:35:27 +0545

    Formatted python file

--- problem-solving/utopian_tree.py
+++ problem-solving/utopian_tree.py
@@ -1,9 +1,8 @@
-
-
 def utopian_tree(n):
     height = 1
-    for i in range(0,n+1):
-        if i==0: continue
+    for i in range(0, n+1):
+        if i == 0:
+            continue
         if i == 1:
             height = height + 1
             continue
@@ -11,10 +10,10 @@ def utopian_tree(n):
             height = height + 1
         else:
             height = height * 2
-            
+
     return height
 
 
-print('0',utopian_tree(0))
-print('1',utopian_tree(1))
-print('4',utopian_tree(4))
+print('0', utopian_tree(0))
+print('1', utopian_tree(1))
+print('4', utopian_tree(4))
